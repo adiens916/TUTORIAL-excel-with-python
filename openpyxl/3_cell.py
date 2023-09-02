e03b82948efa7d03cb5df1cb7335df4f9cf82bd7
Inho <adiens916@naver.com>
2023-09-02 10:24:08 +0900

    Add how to write value and to save

---
+++ openpyxl/3_cell.py
@@ -0,0 +1,21 @@
+import openpyxl as op
+
+path = "./openpyxl"
+
+
+def main():
+    pass
+    write_cell()
+
+
+def write_cell():
+    workbook = op.load_workbook(f"{path}/target.xlsx")  # 객체 생성
+    worksheet = workbook["Sheet1"]  # 객체 생성
+
+    worksheet.cell(1, 1).value = "test 1"  # A1 입력
+    worksheet["C1"].value = "test 2"  # C1 입력
+
+    workbook.save(f"{path}/saved.xlsx")  # 저장
+
+
+main()
