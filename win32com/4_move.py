5859d096e729030e3878e92e1eaa7dcd863a240a
Inho_Gootz <adiens916@naver.com>
2023-09-07 17:46:44 +0900

    Add hint and reference

--- win32com/4_move.py
+++ win32com/4_move.py
@@ -15,3 +15,7 @@ worksheet.Range("A1").Select()  # A1 선택
 worksheet.Range("A1").End(constants.xlDown).Select()  # 맨 밑 A9 선택
 worksheet.Range("A9").End(constants.xlToRight).Select()  # 맨 우측 D9 선택
 print()
+
+
+# Hint: https://www.mrexcel.com/board/threads/vba-shift-up-arrow-after-ctrl-shift-down-arrow.1082629/
+# Ref: https://learn.microsoft.com/en-us/office/vba/api/excel.range.end
