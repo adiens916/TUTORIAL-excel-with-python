f166644b639bed8f40e3b3270ebf74439ca8c5de
Inho_Gootz <adiens916@naver.com>
2023-09-07 18:06:18 +0900

    Refactor running excel part

--- win32com/settings.py
+++ win32com/settings.py
@@ -1,4 +1,15 @@
 import os
+import win32com.client
 
 root = os.getcwd()
 directory = root + "/win32com"
+
+
+def get_active_sheet_of_excel(name: str):
+    excel = win32com.client.gencache.EnsureDispatch("Excel.Application")  # 엑셀 앱 실행
+    excel.Visible = True  # 실행 과정 보이게
+
+    file = f"{directory}/{name}.xlsx"
+    workbook = excel.Workbooks.Open(file)  # 기존에 생성된 문서를 Workbook 객체로 지정
+    worksheet = workbook.ActiveSheet  # 활성화된 시트를 객체로 생성
+    return worksheet
