bf4290763c431f3e74d6c3c584b4acd8c5ca8f26
Inho_Gootz <adiens916@naver.com>
2023-09-07 17:44:51 +0900

    Complete move example

--- win32com/4_move.py
+++ win32com/4_move.py
@@ -1,10 +1,17 @@
 import win32com.client
+from win32com.client import constants
 from settings import directory
 
-excel = win32com.client.Dispatch("Excel.Application")  # 엑셀 앱 실행
+excel = win32com.client.gencache.EnsureDispatch("Excel.Application")  # 엑셀 앱 실행
 excel.Visible = True  # 실행 과정 보이게
 
 file = f"{directory}/4_move.xlsx"
 workbook = excel.Workbooks.Open(file)  # 기존에 생성된 문서를 Workbook 객체로 지정
 worksheet = workbook.ActiveSheet  # 활성화된 시트를 객체로 생성
+print()
+
+
+worksheet.Range("A1").Select()  # A1 선택
+worksheet.Range("A1").End(constants.xlDown).Select()  # 맨 밑 A9 선택
+worksheet.Range("A9").End(constants.xlToRight).Select()  # 맨 우측 D9 선택
 print()
