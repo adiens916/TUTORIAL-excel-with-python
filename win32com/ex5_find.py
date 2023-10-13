6c85411e73e74e7b539ea1cdc967bc68f7595695
Inho_Gootz <adiens916@naver.com>
2023-10-13 15:07:09 +0900

    Fix find example to be wrapped by main function

--- win32com/ex5_find.py
+++ win32com/ex5_find.py
@@ -1,32 +1,32 @@
 from starter import get_active_sheet_of_excel
 
 
-### 1. 셀 한 개 찾기
-worksheet = get_active_sheet_of_excel("ex5_find")  # 엑셀 실행
+def main():
+    ### 1. 셀 한 개 찾기
+    worksheet = get_active_sheet_of_excel("ex5_find")  # 엑셀 실행
 
-target_cell = worksheet.UsedRange.Find("화곡초")  # '화곡초' 검색
-target_cell.Select()  # 선택
+    target_cell = worksheet.UsedRange.Find("화곡초")  # '화곡초' 검색
+    target_cell.Select()  # 선택
 
-target_cell = worksheet.UsedRange.Find("화종초")  # 없는 것 검색
-# target_cell.Select()  # 선택하면 에러 남 (None이라서 Select 속성이 없기 때문)
-print(f"화종초 위치: {target_cell}")
+    target_cell = worksheet.UsedRange.Find("화종초")  # 없는 것 검색
+    # target_cell.Select()  # 선택하면 에러 남 (None이라서 Select 속성이 없기 때문)
+    print(f"화종초 위치: {target_cell}")
 
+    ### 2. 셀 여러 개 찾기
+    found_cell = worksheet.UsedRange.Find("신곡중")
+    if found_cell is not None:
+        first_cell_address = found_cell.Address
 
-### 2. 셀 여러 개 찾기
-found_cell = worksheet.UsedRange.Find("신곡중")
-if found_cell is not None:
-    first_cell_address = found_cell.Address
+        while True:
+            # 다음 셀 찾기
+            # 이때 FindNext 안에 찾을 기준이 되는 셀 (이전 셀)을 넣어야 함
+            # 안 넣으면 하나만 찾고 끝남 (기준이 없으니 매번 좌측 셀 기준으로만 찾기 때문)
+            found_cell = worksheet.UsedRange.FindNext(found_cell)
+            print(f"신곡중 위치: {found_cell.Address}")
 
-    while True:
-        # 다음 셀 찾기
-        # 이때 FindNext 안에 찾을 기준이 되는 셀 (이전 셀)을 넣어야 함
-        # 안 넣으면 하나만 찾고 끝남 (기준이 없으니 매번 좌측 셀 기준으로만 찾기 때문)
-        found_cell = worksheet.UsedRange.FindNext(found_cell)
-        print(f"신곡중 위치: {found_cell.Address}")
-
-        # 만족하는 게 없거나 / 전부 다 찾아서 처음으로 돌아온 경우, 종료
-        if (found_cell is None) or (found_cell.Address == first_cell_address):
-            break
+            # 만족하는 게 없거나 / 전부 다 찾아서 처음으로 돌아온 경우, 종료
+            if (found_cell is None) or (found_cell.Address == first_cell_address):
+                break
 
 
 ### 2.1. 함수화
@@ -46,3 +46,7 @@ def find_cells(worksheet, keyword: str) -> list:
                 break
 
     return found_cells
+
+
+if __name__ == "__main__":
+    main()
