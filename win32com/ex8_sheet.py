4d492fc06c6157828c5dae38da67f8fbe40879fa
Inho_Gootz <adiens916@naver.com>
2023-10-13 11:32:25 +0900

    Add sheet example

---
+++ win32com/ex8_sheet.py
@@ -0,0 +1,18 @@
+from settings import get_excel, get_sheet
+
+
+# 엑셀 실행
+workbook = get_excel("ex8_sheet")
+
+# Sheet1 & Sheet2 가져오기
+sheet1 = get_sheet(workbook, "Sheet1")
+sheet2 = get_sheet(workbook, "Sheet2")
+
+# 각 시트별 데이터 가져오기
+A1 = sheet1.Range("A1")
+B2 = sheet2.Range("B2")
+
+# 해당 셀을 포함하는 시트명 출력
+# 참고: https://learn.microsoft.com/en-us/office/vba/api/excel.range.worksheet
+print(f"A1 셀의 시트명: {A1.Worksheet.Name}")
+print(f"B2 셀의 시트명: {B2.Worksheet.Name}")
--- win32com/settings.py
+++ win32com/settings.py
@@ -13,3 +13,17 @@ def get_active_sheet_of_excel(name: str):
     workbook = excel.Workbooks.Open(file)  # 기존에 생성된 문서를 Workbook 객체로 지정
     worksheet = workbook.ActiveSheet  # 활성화된 시트를 객체로 생성
     return worksheet
+
+
+def get_excel(excel_name: str):
+    excel = win32com.client.gencache.EnsureDispatch("Excel.Application")  # 엑셀 앱 실행
+    excel.Visible = True  # 실행 과정 보이게
+
+    file = f"{directory}/{excel_name}.xlsx"
+    workbook = excel.Workbooks.Open(file)  # 기존에 생성된 문서를 Workbook 객체로 지정
+    return workbook
+
+
+def get_sheet(workbook, sheet_name: str):
+    worksheet = workbook.Worksheets(sheet_name)  # 특정 시트명으로 접근
+    return worksheet
